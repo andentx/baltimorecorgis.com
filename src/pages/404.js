f5b188ac99c6ffe61f5de4d04a2cc3e1c047329b
Andrew D <56039221+andent2@users.noreply.github.com>
2023-02-20 12:17:19 -0500

    Add placeholder 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,49 +1,50 @@
-import * as React from "react"
-import { Link } from "gatsby"
+import * as React from "react";
+import Layout from "../components/Layout";
 
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
+import styled from "styled-components";
+import { Link } from "gatsby";
 
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
+export const Head = () => (
+  <>
+    <title>Baltimore Corgis</title>
+    <meta name='description' content='Baltimore Corgis' />
+    <meta name='author' content='Andrew' />
 
-const NotFoundPage = () => {
+    <meta property='og:title' content='Baltimore Corgis' />
+    <meta property='og:description' content='Baltimore Corgis' />
+    <meta property='og:image' content='' />
+    <meta property='og:image:alt' content='A screenshot of the Baltimore Corgis website' />
+    <meta property='og:locale' content='en_US' />
+    <meta property='og:type' content='website' />
+    <meta property='og:url' content='https://www.baltimorecorgis.com/' />
+
+    <meta name='twitter:card' content='summary' />
+
+    <meta name='theme-color' content='hsl(0, 0%, 0%)' />
+  </>
+);
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  text-align: center;
+`;
+
+const AboutPage = () => {
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry 😔, we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
-  )
-}
+    <>
+      <Layout>
+        <Container>
+          <h1>404</h1>
+          <h2>Page not found</h2>
+          <Link to='/'>Go home</Link>
+        </Container>
+      </Layout>
+    </>
+  );
+};
 
-export default NotFoundPage
-
-export const Head = () => <title>Not found</title>
+export default AboutPage;
