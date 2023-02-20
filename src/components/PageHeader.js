191bb44d0540a3a26adce18f6185c66101b05f29
Andrew D <56039221+andent2@users.noreply.github.com>
2023-02-20 12:21:42 -0500

    Add page header

---
+++ src/components/PageHeader.js
@@ -0,0 +1,32 @@
+import * as React from 'react';
+
+import styled from 'styled-components';
+
+import PageHeaderLogo from './PageHeaderLogo';
+import PageNavigation from './Navigation/PageNavigation';
+
+const Header = styled.header`
+  background-color: var(--page-header-background-color);
+  color: var(--text-color);
+
+  height: 4rem;
+  width: 100%;
+  max-width: 1200px;
+
+  padding: 0.5rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const PageHeader = () => {
+  return (
+    <Header>
+      <PageHeaderLogo />
+      <PageNavigation />
+    </Header>
+  );
+};
+
+export default PageHeader;
