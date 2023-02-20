32bc178895f00ec475585a20126ef53efab38db0
Andrew D <56039221+andent2@users.noreply.github.com>
2023-02-20 17:44:30 -0500

    Implement color variables

---
+++ src/components/Color.js
@@ -0,0 +1,57 @@
+import * as React from "react";
+
+import { createGlobalStyle } from "styled-components";
+
+const GlobalColor = createGlobalStyle`
+
+  :root {
+    /* primary color */
+    --baltimore-purple: hsl(264, 36%, 31%);
+    
+    /* black */
+    --off-black: hsl(180, 2%, 8%);
+
+    /* white */
+    --off-white: hsl(0, 0%, 99%);
+    --off-white-darker: hsl(0, 0%, 70%);
+
+    /* color roles */
+    --site-background-color: var(--off-black);
+    
+    --text-color: var(--off-white);
+    --text-color-hover: var(--off-white-darker);
+
+    --accent-color: hotpink;
+    --focus-color: var(--off-white-darker);
+    
+    /* color assignments */
+    --page-header-background-color: var(--baltimore-purple);
+
+    --page-header-logo-color: red;
+
+    --nav-menu-open-icon-color: var(--off-white);
+    --nav-menu-close-icon-color: var(--off-white);
+
+    --main-section-background-color: var(--off-black);
+
+    --page-footer-background-color: var(--baltimore-purple);
+  }
+
+  html {
+    background-color: var(--baltimore-purple);
+  }
+  body {
+    background-color: var(--site-background-color);
+  }
+
+ `;
+
+const Color = () => {
+  return (
+    <>
+      <GlobalColor />
+    </>
+  );
+};
+
+export default Color;
