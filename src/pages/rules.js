cdbe81d48e3a446872c02898f42fcb8dfddb3ad8
Andrew D <56039221+andent2@users.noreply.github.com>
2023-02-22 21:19:57 -0500

    Add placeholder pages

---
+++ src/pages/rules.js
@@ -0,0 +1,35 @@
+import * as React from 'react';
+
+import Layout from '../components/Layout';
+
+export const Head = () => (
+  <>
+    <title>Baltimore Corgis | Rules</title>
+    <meta name='description' content='Baltimore Corgis' />
+    <meta name='author' content='Andrew' />
+
+    <meta property='og:title' content='Baltimore Corgis' />
+    <meta property='og:description' content='Baltimore Corgis' />
+    <meta property='og:image' content='' />
+    <meta property='og:image:alt' content='A screenshot of the Baltimore Corgis website' />
+    <meta property='og:locale' content='en_US' />
+    <meta property='og:type' content='website' />
+    <meta property='og:url' content='https://www.baltimorecorgis.com/' />
+
+    <meta name='twitter:card' content='summary' />
+
+    <meta name='theme-color' content='hsl(264, 36%, 31%)' />
+  </>
+);
+
+const RulesPage = () => {
+  return (
+    <>
+      <Layout>
+        <h1>Rules</h1>
+      </Layout>
+    </>
+  );
+};
+
+export default RulesPage;
