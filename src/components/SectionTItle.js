bce02d80209a0a7541b4d7755c9e1bcbd526a8d7
Andrew D <56039221+andent2@users.noreply.github.com>
2023-02-22 22:44:02 -0500

    Remove testing color

--- src/components/SectionTItle.js
+++ src/components/SectionTItle.js
@@ -3,7 +3,6 @@ import * as React from 'react';
 import styled from 'styled-components';
 
 const Title = styled.h2`
-  color: lime;
   font-size: 3rem;
   text-align: center;
   margin-bottom: 1rem;
