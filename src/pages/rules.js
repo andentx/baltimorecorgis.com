248d00c1c53d10339951aa4bb8349b70d17874bb
Sarah H <126206574+SarahHammond314@users.noreply.github.com>
2023-02-23 20:57:05 -0500

    Update rules data

--- src/pages/rules.js
+++ src/pages/rules.js
@@ -96,14 +96,57 @@ const groupRules = [
   },
   {
     id: 3,
-    title: 'No Corgi for Sale Posts',
-    description: '"For Sale" or "Selling" posts will not be permitted. Posts may only permit re-homing or adoption',
+    title: 'In Search of Corgi Posts',
+    description: 'ISO posts may not be allowed commenting in order to avoid publicizing the promotion of breeders. Do not use language that promotes buying, selling, or breeders in your post or it will be denied.',
   },
   {
     id: 4,
-    title: 'No Corgi for Sale Posts',
-    description: '"For Sale" or "Selling" posts will not be permitted. Posts may only permit re-homing or adoption',
+    title: 'Breeders',
+    description: 'You may not publicize or promote puppies, new litters, or your business. Selling animals is against Facebook rules.',
   },
+  {
+    id: 5,
+    title: 'Corgi Only Promotions',
+    description: 'Promotions are only allowed if they are supporting a local business and are specifically Corgi related. (i.e. Your corgis Instagram or Facebook page, Etsy for Corgi items, or Corgi-related events.)',
+  },
+  {
+    id: 6,
+    title: 'No Stud Posts',
+    description: 'Posts asking where to find or in search of a breeding mate will not be permitted.',
+  },
+  {
+    id: 7,
+    title: 'Keep Political Views to Yourself',
+    description: 'Please keep political views on your own page. This is not the place to discuss it.',
+  },
+  {
+    id: 8,
+    title: 'Be Kind and Courteous',
+    description: 'This is self-explanatory. Just be nice!',
+  },
+  {
+    id: 9,
+    title: 'No Hate Speech or Bullying',
+    description: 'Bullying of any kind is not allowed, and degrading comments about things like race, religion, culture, sexual orientation, gender, or identity will not be tolerated.',
+  },
+];
+const eventRules = [
+  {
+    id: 1,
+    title: 'No Food or Dog Treats',
+    description: 'Please no human food or dog treats. Handing out treats may cause unnecessary conflict. Some dogs may also have allergies or diet restrictions.',
+  },
+  {
+    id: 2,
+    title: 'No Toys or Balls',
+    description: 'Please refrain from playing with toys or balls. Some dogs are possessive over toys/balls and this causes unnecessary conflict.',
+  },
+  {
+    id: 3,
+    title: 'Clean Up After Your Dog',
+    description: 'Please keep an eye on your dog and clean up after them. In most cases, there will be supplies such as paper towels, spray, poop bags, trash cans, etc.',
+  },
+
 ];
 
 const RulesPage = () => {
@@ -130,7 +173,7 @@ const RulesPage = () => {
         <SectionTitle>Event Rules</SectionTitle>
         <VerticalSpacer />
 
-        {groupRules.map((rule) => (
+        {eventRules.map((rule) => (
           <Rule key={rule.id}>
             <NumberContainer>
               <p>{rule.id}</p>
