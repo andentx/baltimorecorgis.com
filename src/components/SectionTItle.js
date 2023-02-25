d9201c38766b627a5e5e6dabed50ddff4560e212
Andrew D <56039221+andent2@users.noreply.github.com>
2023-02-25 12:14:02 -0500

    Update section title margin

--- src/components/SectionTItle.js
+++ src/components/SectionTItle.js
@@ -5,9 +5,8 @@ import styled from 'styled-components';
 const Title = styled.h2`
   font-size: 3rem;
   text-align: center;
-  margin-bottom: 1rem;
 
-  margin-left: 2rem;
+  margin: 0 1.5rem 1rem 1.5rem;
 
   font-family: 'Gill Sans Custom';
 `;
