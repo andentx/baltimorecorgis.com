e4bd09e2bd76bf792140042ec775c00c751ae788
Andrew D <56039221+andent2@users.noreply.github.com>
2023-03-01 10:38:10 -0500

    Remove font stack from html

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -50,7 +50,6 @@ const GlobalReset = createGlobalStyle`
  }
 
  html {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
     overflow-x: hidden;
 }
  `;
