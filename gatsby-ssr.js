cf72f8ae27930cefe24b1b935c83adb3fa1d7836
Andrew D <56039221+andent2@users.noreply.github.com>
2023-03-01 14:36:54 -0500

    Remove duplicate theme color

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,6 +5,5 @@ export const onRenderBody = ({ setHeadComponents }) => {
     <link rel='preload' href='/fonts/GillSansCustom.woff2' as='font' type='font/woff2' crossOrigin='anonymous' key='gillSansCustomFont' />,
     <link rel='preload' href='/fonts/MontserratLight.woff2' as='font' type='font/woff2' crossOrigin='anonymous' key='montserratLightFont' />,
     <link rel='preload' href='/fonts/MontserratRegular.woff2' as='font' type='font/woff2' crossOrigin='anonymous' key='montserratRegularFont' />,
-    <meta name='theme-color' content='hsl(264, 36%, 31%)' />,
   ]);
 };
