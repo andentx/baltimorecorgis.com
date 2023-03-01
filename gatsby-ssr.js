787bb9e59fa8f4a1535ceff09df4ebda2a795307
Andrew D <56039221+andent2@users.noreply.github.com>
2023-03-01 11:11:20 -0500

    Pre-load fonts with ssr

---
+++ gatsby-ssr.js
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export const onRenderBody = ({ setHeadComponents }) => {
+  setHeadComponents([
+    <link rel='preload' href='/fonts/GillSansCustom.woff2' as='font' type='font/woff2' crossOrigin='anonymous' key='gillSansCustomFont' />,
+    <link rel='preload' href='/fonts/MontserratLight.woff2' as='font' type='font/woff2' crossOrigin='anonymous' key='montserratLightFont' />,
+    <link rel='preload' href='/fonts/MontserratRegular.woff2' as='font' type='font/woff2' crossOrigin='anonymous' key='montserratRegularFont' />,
+  ]);
+};
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -9,11 +9,6 @@ import Color from '../components/Color';
 import PageHeader from '../components/PageHeader';
 import PageFooter from '../components/PageFooter';
 
-// import GillSansCustom from '../../static/fonts/GillSansCustom.woff2';
-import GillSansCustom from '../fonts/GillSansCustom.woff2';
-import MontserratRegular from '../fonts/MontserratRegular.woff2';
-import MontserratLight from '../fonts/MontserratLight.woff2';
-
 const GlobalReset = createGlobalStyle`
  html,
  body,
@@ -57,20 +52,21 @@ const GlobalReset = createGlobalStyle`
  `;
 
 const FontStyles = createGlobalStyle`
-@font-face {
-  font-family: 'Gill Sans Custom';
-  src: url(${GillSansCustom}) format('woff2');
-}
-
   @font-face {
-  font-family: 'Montserrat Regular';
-  src: url(${MontserratRegular}) format('woff2');
-}
-
+    font-family: 'Gill Sans Custom';
+    /* font-display: swap; */
+    src: url(/fonts/GillSansCustom.woff2) format("woff2");
+  }
   @font-face {
-  font-family: 'Montserrat Light';
-  src: url(${MontserratLight}) format('woff2');
-}
+    font-family: 'Montserrat Light';
+    /* font-display: swap; */
+    src: url(/fonts/MontserratLight.woff2) format("woff2");
+  }
+  @font-face {
+    font-family: 'Montserrat Regular';
+    /* font-display: swap; */
+    src: url(/fonts/MontserratRegular.woff2) format("woff2");
+  }
 `;
 
 const PageContainer = styled.div`
