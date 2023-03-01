04030aa7eed3c749320948f9527b364ea4bd4cac
Andrew D <56039221+andent2@users.noreply.github.com>
2023-03-01 11:47:55 -0500

    Revert fonts

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -9,6 +9,10 @@ import Color from '../components/Color';
 import PageHeader from '../components/PageHeader';
 import PageFooter from '../components/PageFooter';
 
+import GillSansCustom from '../fonts/GillSansCustom.woff2';
+import MontserratRegular from '../fonts/MontserratRegular.woff2';
+import MontserratLight from '../fonts/MontserratLight.woff2';
+
 const GlobalReset = createGlobalStyle`
  html,
  body,
@@ -51,18 +55,18 @@ const GlobalReset = createGlobalStyle`
  `;
 
 const FontStyles = createGlobalStyle`
+@font-face {
+  font-family: 'Gill Sans Custom';
+  src: url(${GillSansCustom}) format('woff2');
+}
   @font-face {
-    font-family: 'Gill Sans Custom';
-    src: url(/fonts/GillSansCustom.woff2) format("woff2");
-  }
+  font-family: 'Montserrat Regular';
+  src: url(${MontserratRegular}) format('woff2');
+}
   @font-face {
-    font-family: 'Montserrat Light';
-    src: url(/fonts/MontserratLight.woff2) format("woff2");
-  }
-  @font-face {
-    font-family: 'Montserrat Regular';
-    src: url(/fonts/MontserratRegular.woff2) format("woff2");
-  }
+  font-family: 'Montserrat Light';
+  src: url(${MontserratLight}) format('woff2');
+}
 `;
 
 const PageContainer = styled.div`
