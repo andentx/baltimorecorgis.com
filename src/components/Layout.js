e6a0d169ff45acfe78bd55f898ff2b1a4e243e68
Andrew D <56039221+andent2@users.noreply.github.com>
2023-03-01 12:12:53 -0500

    Implement fonts with gatsby-ssr and global.css

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -9,10 +9,6 @@ import Color from '../components/Color';
 import PageHeader from '../components/PageHeader';
 import PageFooter from '../components/PageFooter';
 
-import GillSansCustom from '../fonts/GillSansCustom.woff2';
-import MontserratRegular from '../fonts/MontserratRegular.woff2';
-import MontserratLight from '../fonts/MontserratLight.woff2';
-
 const GlobalReset = createGlobalStyle`
  html,
  body,
@@ -54,21 +50,6 @@ const GlobalReset = createGlobalStyle`
 }
  `;
 
-const FontStyles = createGlobalStyle`
-@font-face {
-  font-family: 'Gill Sans Custom';
-  src: url(${GillSansCustom}) format('woff2');
-}
-  @font-face {
-  font-family: 'Montserrat Regular';
-  src: url(${MontserratRegular}) format('woff2');
-}
-  @font-face {
-  font-family: 'Montserrat Light';
-  src: url(${MontserratLight}) format('woff2');
-}
-`;
-
 const PageContainer = styled.div`
   min-height: 100vh;
   min-height: 100dvh;
@@ -105,7 +86,6 @@ const Layout = ({ children }) => {
     <>
       <GlobalReset />
       <Color />
-      <FontStyles />
       <PageContainer>
         <PageHeader />
         <MainContent>{children}</MainContent>
