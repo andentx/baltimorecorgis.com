00659b6550e93e4a633aa40284445fa247605510
Andrew D <56039221+andent2@users.noreply.github.com>
2023-03-04 21:24:48 -0500

    Add Chunk Five font

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,5 +5,6 @@ export const onRenderBody = ({ setHeadComponents }) => {
     <link rel='preload' href='/fonts/GillSansCustom.woff2' as='font' type='font/woff2' crossOrigin='anonymous' key='gillSansCustomFont' />,
     <link rel='preload' href='/fonts/MontserratLight.woff2' as='font' type='font/woff2' crossOrigin='anonymous' key='montserratLightFont' />,
     <link rel='preload' href='/fonts/MontserratRegular.woff2' as='font' type='font/woff2' crossOrigin='anonymous' key='montserratRegularFont' />,
+    <link rel='preload' href='/fonts/ChunkFive.woff2' as='font' type='font/woff2' crossOrigin='anonymous' key='chunkFiveFont' />,
   ]);
 };
