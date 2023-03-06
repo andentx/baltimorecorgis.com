294e0ef3b496c7154ab079408a7b5c3cdcca195a
Andrew D <56039221+andent2@users.noreply.github.com>
2023-03-06 17:05:01 -0500

    Add sorting to events

--- src/data/events.js
+++ src/data/events.js
@@ -37,8 +37,65 @@ export const allEvents = [
     buttonText: 'FREE',
     photo: <StaticImage src='../images/dogs/cooper.jpg' height={180} width={180} alt={`A photo of Cooper, a red and white Pembroke Welsh Corgi.`} />,
   },
+  {
+    id: 3,
+    title: "Cooper's Ugly Sweater Birthday",
+    date: '2023-06-15',
+    month: 'JUN',
+    day: '15',
+    year: '2023',
+    dateString: 'June 13th, 2023',
+    startTime: '11:30 AM',
+    endTime: '12:00 PM',
+    venue: 'A Very Extremely Long Venue Name',
+    city: 'Havre de Grace',
+    state: 'MD',
+    admissionPrice: 'Free',
+    buttonText: 'FREE',
+    photo: <StaticImage src='../images/dogs/cooper.jpg' height={180} width={180} alt={`A photo of Cooper, a red and white Pembroke Welsh Corgi.`} />,
+  },
+  {
+    id: 4,
+    title: "Cooper's Ugly Sweater Birthday",
+    date: '2023-08-01',
+    month: 'AUG',
+    day: '01',
+    year: '2023',
+    dateString: 'June 13th, 2023',
+    startTime: '11:30 AM',
+    endTime: '12:00 PM',
+    venue: 'A Very Extremely Long Venue Name',
+    city: 'Havre de Grace',
+    state: 'MD',
+    admissionPrice: 'Free',
+    buttonText: 'FREE',
+    photo: <StaticImage src='../images/dogs/cooper.jpg' height={180} width={180} alt={`A photo of Cooper, a red and white Pembroke Welsh Corgi.`} />,
+  },
+  {
+    id: 5,
+    title: "Cooper's Ugly Sweater Birthday",
+    date: '2023-01-13',
+    month: 'JAN',
+    day: '13',
+    year: '2023',
+    dateString: 'June 13th, 2023',
+    startTime: '11:30 AM',
+    endTime: '12:00 PM',
+    venue: 'A Very Extremely Long Venue Name',
+    city: 'Havre de Grace',
+    state: 'MD',
+    admissionPrice: 'Free',
+    buttonText: 'FREE',
+    photo: <StaticImage src='../images/dogs/cooper.jpg' height={180} width={180} alt={`A photo of Cooper, a red and white Pembroke Welsh Corgi.`} />,
+  },
 ];
 
 const now = Date.now();
-export const upcomingEvents = allEvents.filter((event) => new Date(event.date) >= now);
-export const pastEvents = allEvents.filter((event) => new Date(event.date) < now);
+export const upcomingEvents = allEvents.filter((event) => new Date(event.date) >= now).sort((a, b) => new Date(a.date) - new Date(b.date));
+
+export const pastEvents = allEvents.filter((event) => new Date(event.date) < now).sort((a, b) => new Date(b.date) - new Date(a.date));
+
+export const nextThreeEvents = allEvents
+  .filter((event) => new Date(event.date) >= now)
+  .sort((a, b) => new Date(a.date) - new Date(b.date))
+  .slice(0, 3);
