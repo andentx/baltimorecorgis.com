41af6d3687fc280ddbf1314e0a0744400d0080ba
Andrew D <56039221+andent2@users.noreply.github.com>
2024-02-02 15:50:37 -0500

    Add redirect to highlight

--- src/pages/highlight.js
+++ src/pages/highlight.js
@@ -1,109 +1,12 @@
-import * as React from "react";
-import { Link } from "gatsby";
-import styled from "styled-components";
-import Layout from "../components/Layout";
-import SectionTitle from "../components/SectionTitle.js";
+import { useEffect } from "react";
 
-export const Head = () => (
-  <>
-    <title>Baltimore Corgis</title>
-    <meta
-      name="description"
-      content="Established in 2014, we're a community of Corgi owners based in the Baltimore area and surrounding counties."
-    />
-    <meta name="author" content="Andrew" />
+const HighlightRedirect = () => {
+  useEffect(() => {
+    window.location.href =
+      "https://docs.google.com/forms/d/e/1FAIpQLScuZM7H0AbcYEx2f4Nbyol46tobRUrzx5PXgQcy720ynd_RYw/viewform";
+  }, []);
 
-    <meta property="og:title" content="Baltimore Corgis" />
-    <meta
-      property="og:description"
-      content="Established in 2014, we're a community of Corgi owners based in the Baltimore area and surrounding counties."
-    />
-    <meta
-      property="og:image"
-      content="https://baltimorecorgis.com/images/og-image.png"
-    />
-    <meta
-      property="og:image:alt"
-      content="A screenshot of the Baltimore Corgis website"
-    />
-    <meta property="og:locale" content="en_US" />
-    <meta property="og:type" content="website" />
-    <meta property="og:url" content="https://baltimorecorgis.com/" />
-
-    <meta name="twitter:card" content="summary_large_image" />
-
-    <meta name="theme-color" content="hsl(264, 36%, 31%)" />
-  </>
-);
-
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-
-  text-align: center;
-
-  color: var(--text-color);
-
-  transition: color 200ms;
-
-  a {
-    color: var(--text-color);
-
-    :link {
-      color: var(--text-color);
-    }
-
-    :visited {
-      color: var(--text-color);
-    }
-
-    :hover {
-      color: hsl(0, 0%, 70%);
-    }
-
-    :active {
-      color: var(--text-color);
-    }
-
-    padding: 0.5rem 1rem;
-    position: relative;
-
-    :focus-visible {
-      outline: none;
-    }
-
-    :focus-visible::before {
-      content: "";
-      position: absolute;
-      top: -2px;
-      right: -2px;
-      bottom: -2px;
-      left: -2px;
-      border: 2px solid var(--focus-color);
-      border-radius: 10px;
-    }
-  }
-
-  h2 {
-    margin-bottom: 2rem;
-  }
-`;
-
-const AboutPage = () => {
-  return (
-    <>
-      <Layout>
-        <Container>
-          <SectionTitle>Highlight Signup</SectionTitle>
-          <Link to="https://docs.google.com/forms/d/e/1FAIpQLScuZM7H0AbcYEx2f4Nbyol46tobRUrzx5PXgQcy720ynd_RYw/viewform">
-            Click to here go to Highlight Signup page
-          </Link>
-        </Container>
-      </Layout>
-    </>
-  );
+  return null;
 };
 
-export default AboutPage;
+export default HighlightRedirect;
